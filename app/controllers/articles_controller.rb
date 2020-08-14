43aab8350708ca22b6f122c0cfad3ebf752556b4
Hessam Mir Mohammadi <hessammir1999@gmail.com>
2020-08-14 20:13:30 +0100

    added all of the links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,8 +18,6 @@ class ArticlesController < ApplicationController
 
   end
 
- 
-
   def create
     @article = Article.new(params.require(:article).permit(:title,:description))
     if @article.save
@@ -41,5 +39,11 @@ class ArticlesController < ApplicationController
       render 'edit'
 
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   
   
-  # this gives all the routes we want for articles
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # this gives all the routes we want for articles, we can take the only's off if we want to use all of the routes
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update,:destroy]
 end
