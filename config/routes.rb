f477872d7c98bd947b21623c7da393431d69d84d
Hessam Mir Mohammadi <hessammir1999@gmail.com>
2020-08-22 17:31:35 +0100

    added the sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   
-  
   # this gives all the routes we want for articles, we can take the only's off if we want to use all of the routes
   resources :articles, only: [:show, :index, :new, :create, :edit, :update,:destroy]
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 end
