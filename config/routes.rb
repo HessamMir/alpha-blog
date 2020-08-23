d3ead04ed3f689512b1539c7ba4990aa9d6a8708
Hessam Mir Mohammadi <hessammir1999@gmail.com>
2020-08-24 00:43:25 +0100

    added update to edit in users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   
   # this gives all the routes we want for articles, we can take the only's off if we want to use all of the routes
   resources :articles, only: [:show, :index, :new, :create, :edit, :update,:destroy]
+  resources :users
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
+
 end
