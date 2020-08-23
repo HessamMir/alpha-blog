6d9b1212312d44f55798b9d28216bc52ad6ae179
Hessam Mir Mohammadi <hessammir1999@gmail.com>
2020-08-24 00:07:49 +0100

    add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,20 @@ class UsersController < ApplicationController
     
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "welcome #{@user.username}, you have succesfully signed up!"
+      redirect_to articles_path
+    else
+      render 'new'
+  end
+end
+
+  private 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 
 end
